bc5abb3b38510c46c99fc8a678c465029085f7c4
akitomonam <akitomonam@gmail.com>
2022-10-06 16:05:35 +0900

    接続情報をファイル形式に変更

--- client.py
+++ client.py
@@ -1,9 +1,17 @@
 import socket
 
+def read_json_file(json_file_path: str)->list:
+    data = json.load(open(
+        json_file_path, 'r'))
+    return data
 
 def main():
+    config_path = "./config.json"
+    data = read_json_file(config_path)
+    ip_addr = data["server_ip"]
+    port_num = data["port"]
     soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    soc.connect(('172.21.65.74', 12345))
+    soc.connect((ip_addr, port_num))
 
     while(1):
         data = input("Client>")  # 入力待機
